c2965386254d75ea860cc08792b73b1f2c39016a
MLev98 <levinsonmichael1@gmail.com>
2021-03-08 18:25:59 -0500

    Styling for Profile & Initial JavaScript
    * Text Auto Alignment for CSS File
    * Text Auto Alignment for JS File
    * Adds a new container class called "container2" which is wider than the original.
    * Centers Profile Fields
    
    * (WIP) checkbox strikethrough function, need help on this one

--- src/main.js
+++ src/main.js
@@ -1,98 +1,123 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 /**
- * Displays Message for errors or success
- *
- * @param formElement Form that needs message associated with it
- * @param type Type of message, error or success
- * @param message The message
- */
+* Displays Message for errors or success
+*
+* @param formElement Form that needs message associated with it
+* @param type Type of message, error or success
+* @param message The message
+*/
 function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form__message");
+  const messageElement = formElement.querySelector(".form__message");
 
-    messageElement.textContent = message;
-    messageElement.classList.remove("form__message--success", "form__message--error");
-    messageElement.classList.add(`form__message--${type}`);
+  messageElement.textContent = message;
+  messageElement.classList.remove("form__message--success", "form__message--error");
+  messageElement.classList.add(`form__message--${type}`);
 }
 
 /**
- * Prints error messages for designated input fields
- *
- * @param inputElement Input field that needs error
- * @param message Error message
- */
+* Prints error messages for designated input fields
+*
+* @param inputElement Input field that needs error
+* @param message Error message
+*/
 function setInputError(inputElement, message) {
-    inputElement.classList.add("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+  inputElement.classList.add("form__input--error");
+  inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
 }
 
 /**
- * Clears error messages for designated input fields
- *
- * @param inputElement Input field that error will be cleared from
- */
+* Clears error messages for designated input fields
+*
+* @param inputElement Input field that error will be cleared from
+*/
 function clearInputError(inputElement) {
-    inputElement.classList.remove("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+  inputElement.classList.remove("form__input--error");
+  inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
 }
 
 /**
- * Toggles between form pages to be seen and the other hidden
- *
- * @param form1 Form to be hidden
- * @param form2 Form to be shown
- */
+* Toggles between form pages to be seen and the other hidden
+*
+* @param form1 Form to be hidden
+* @param form2 Form to be shown
+*/
 function formVisibilityToggle(form1, form2) {
   form1.classList.add("form--hidden");
   form2.classList.remove("form--hidden");
 }
 
 /**
- * Logins using a test account Test Method
- *
- */
+* Logins using a test account Test Method
+*
+*/
 function login() {
   if (loginUser.value === "admin" && loginCredentials.value === "admin")
-    window.location = "./profile.html";
+  window.location = "./profile.html";
 }
 
 /**
- * Lougouts Test Method
- *
- */
- function logout() {
-   window.location = "./index.html";
- }
+* Lougouts Test Method
+*
+*/
+function logout() {
+  window.location = "./index.html";
+}
+
+/**
+* Makes the checkboxed row strikethroughed and input disabled
+*
+* @param element element affected
+*/
+function strikethroughRow(element) {
+  if (element.is(":checked")) {
+    $(element.name).classList.add("checkCross");
+
+    $(element.value)_instructor.attr('disabled', 'disabled');
+    $(element.value)_instructor.value = " ";
+
+    $(element.value)_grade.attr('dissabled', 'disabled');
+    $(element.value)_grade.value = " ";
+  }
+  else {
+    $(element.name).classList.remove("checkCross");
+
+    $(element.value)_instructor.attr('disabled');
+
+    $(element.value)_grade.attr('dissabled');
+  }
+  // Needs custom to work for any _instructor/grade so JQuery
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
-    const forgotForm = document.querySelector("#forgot");
+  const loginForm = document.querySelector("#login");
+  const createAccountForm = document.querySelector("#createAccount");
+  const forgotForm = document.querySelector("#forgot");
 
-    //Checks for successful login and prints error if unsuccessful
-    loginForm.addEventListener("submit", e => {
-        e.preventDefault();
+  //Checks for successful login and prints error if unsuccessful
+  loginForm.addEventListener("submit", e => {
+    e.preventDefault();
 
-        //Set up success login feature here
+    //Set up success login feature here
 
-        setFormMessage(loginForm, "error", "Invalid username/password");
+    setFormMessage(loginForm, "error", "Invalid username/password");
+  });
+
+  //Checks if input element fulfills specific requirements
+  document.querySelectorAll(".form__input").forEach(inputElement => {
+    inputElement.addEventListener("blur", e => {
+      //Checks that username fulfills requirements for valid username
+      if(e.target.id === "usernameSetUp" && e.target.value.length > 0 && e.target.value.length < 10)
+      //Prints error
+      setInputError(inputElement, "Username must be 10 characters long");
     });
 
-    //Checks if input element fulfills specific requirements
-    document.querySelectorAll(".form__input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            //Checks that username fulfills requirements for valid username
-            if(e.target.id === "usernameSetUp" && e.target.value.length > 0 && e.target.value.length < 10)
-                //Prints error
-                setInputError(inputElement, "Username must be 10 characters long");
-        });
-
-        //Clears error message while typing
-        inputElement.addEventListener("input", e => {
-            clearInputError(inputElement);
-        });
+    //Clears error message while typing
+    inputElement.addEventListener("input", e => {
+      clearInputError(inputElement);
     });
+  });
 });
