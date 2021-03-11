9de3d5e4e40d6a349028a4da9e92791cf79a69f4
MLev98 <levinsonmichael1@gmail.com>
2021-03-11 11:21:08 -0500

    Whitespaces
    * Fixes whitespaces
    * Adds lang to HTML

--- src/dashboard.php
+++ src/dashboard.php
@@ -3,37 +3,37 @@ session_start();
 include("config.php");
 
 if ((!isset($_SESSION['logged_in']))) {
-    header("Location: index.php");
+  header("Location: index.php");
 }
 ?>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>BK College Course Planner</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <title>Brooklyn College Course Planner</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Menu</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="./dashboard.php">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="./Profile.html">Profile</a>
-      <a class="nav-item nav-link" href="./Courses.html">Courses</a>
-      <a class="nav-item nav-link" href="./Planner.html">Planner</a>
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="./dashboard.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="./Profile.html">Profile</a>
+        <a class="nav-item nav-link" href="./Courses.html">Courses</a>
+        <a class="nav-item nav-link" href="./Planner.html">Planner</a>
+      </div>
     </div>
-    </div>
-    </nav>
-<h1 id = "welcome-msg">Welcome! <?php echo $_SESSION['username']?></h1>
-<p>
-  <a href="./profile.html">Edit Your Profile</a>
-</p>
-<button type="button" onclick="location.href='logout.php';">LOGOUT</button>
+  </nav>
+  <h1 id = "welcome-msg">Welcome! <?php echo $_SESSION['username']?></h1>
+  <p>
+    <a href="./profile.html">Edit Your Profile</a>
+  </p>
+  <button type="button" onclick="location.href='logout.php';">LOGOUT</button>
 
 </body>
 </html>
