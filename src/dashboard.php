4256724a2e7c0bdee5cdf40c6082f688c743fdfc
MLev98 <levinsonmichael1@gmail.com>
2021-03-11 11:23:14 -0500

    Fixes charset in dashboard
    * Charset is case sensitive according to the browser, so fixing this here as well.

--- src/dashboard.php
+++ src/dashboard.php
@@ -8,10 +8,10 @@ if ((!isset($_SESSION['logged_in']))) {
 ?>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>BK College Course Planner</title>
+    <title>Brooklyn College Course Planner</title>
 </head>
 <body>
 <h1 id = "welcome-msg">Welcome! <?php echo $_SESSION['username']?></h1>
