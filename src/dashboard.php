87b2e84d1df9754a88e9f0012a31b245e6e632a1
Cassidy McAllister <cgmcallister98@gmail.com>
2021-04-10 22:37:16 -0400

    Menu bar (#4)
    * Added Menu Bar
    
    Added menu bar from bootstrap, need to style profile one. Added two new pages and need to work on them
    
    * Whitespaces
    
    * Fixes whitespaces
    * Adds lang to HTML
    
    * Profile MenuBar Fix
    
    * Fixes MenuBar in profile.html
    * Removes Edit link in Dashboard
    
    * Fixes Active MenuBar
    
    * Fixes the MenuBar for when a page is active, will properly display now!
    
    * Undecided
    
    * Renames Undeclared to Undecided.
    * Undecided will be the default option selected.
    
    * Profile Spacing
    
    * Adds some spacing after the navbar and between the save button and logout link.
    
    * Validated Profile
    
    * Removed duplicate title in profile.
    * Did some validation on JavaScript.
    * Profile is fully validated.
    
    * Fully Validated Index
    
    * Index.php now fully validated
    
    * Courses & Planner (As Is)
    
    * Validated Courses
    * Validated Planner
    
    * Validated Dashboard
    
    * Dashboard (html) validated.
    
    * Revert some Validation in JavaScript
    
    * This will be moved into Validation, don't want to cause problems.
    
    * JavaScript Validation #1
    
    * Some validation fixes on the JavaScript, this does not fix them all, there is still quite a few left (I can't find a workaround).
    
    * Excel Sheet
    
    * Removes the Table Header
    * Strips all the formatting
    * Removes Prerequisites as that would be a rather more advanced option (work on later)
    
    * What is left = Course ID, Course Name, Course Description, Course Credits, Is Course Elective?
    
    * Removes Discussed Pages
    
    * Removes Courses.html
    * Removes Planner.html
    * Removes respective links on MenuBar to removed pages.
    
    * Resize EMPLID Field on Profile
    
    * Resizes the EMPLID field on the Profile Page
    * Some CSS Minor Whitespaces
    
    Co-authored-by: MLev98 <levinsonmichael1@gmail.com>
    Co-authored-by: MLev98 <58785503+MLev98@users.noreply.github.com>

--- src/dashboard.php
+++ src/dashboard.php
@@ -3,22 +3,33 @@ session_start();
 include("config.php");
 
 if ((!isset($_SESSION['logged_in']))) {
-    header("Location: index.php");
+  header("Location: index.php");
 }
 ?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Brooklyn College Course Planner</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <title>Brooklyn College Course Planner</title>
 </head>
 <body>
-<h1 id = "welcome-msg">Welcome! <?php echo $_SESSION['username']?></h1>
-<p>
-  <a href="./profile.html">Edit Your Profile</a>
-</p>
-<button type="button" onclick="location.href='logout.php';">LOGOUT</button>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Menu</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="./dashboard.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="./profile.html">Profile</a>
+      </div>
+    </div>
+  </nav>
+  <h1 id = "welcome-msg">Welcome! <?php echo $_SESSION['username']?></h1>
+  <button type="button" onclick="location.href='logout.php';">LOGOUT</button>
 
 </body>
 </html>
