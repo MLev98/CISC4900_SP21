7ff84aa840dbb3771b53d0be47cd145483cd5c22
lrojas998 <61262142+lrojas998@users.noreply.github.com>
2021-04-10 22:05:03 -0400

    Added endpoints for drag/drop mobility

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,24 +1,31 @@
 <?php
 session_start();
+ob_start();
 include("config.php");
 
-if ((!isset($_SESSION['logged_in']))) {
-    header("Location: index.php");
-}
 ?>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Brooklyn College Course Planner</title>
-</head>
-<body>
-<h1 id = "welcome-msg">Welcome! <?php echo $_SESSION['username']?></h1>
-<p>
-  <a href="./profile.html">Edit Your Profile</a>
-</p>
-<button type="button" onclick="location.href='logout.php';">LOGOUT</button>
+<!-- jQuery min js -->
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+<!-- jQuery ui js -->
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<!-- BOOTSTRAP CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
-</body>
+<link rel="stylesheet" href="sortable.css">
+<script src="sortable.js"></script>
+
+<!-- ADD A COURSE FORM -->
+<?php include('endpoints/addCourseEndpoint.php');?>
+
+<!-- UPDATE MAJOR FORM -->
+<?php include('endpoints/majorsEndpoint.php');?>
+
+<!-- DISPLAY/UPDATE COURSES-->
+<div id = "container"> 
+<?php include('endpoints/coursesTakenEndpoint.php'); ?>
+</div>
+
+<button onclick="createSemester('addCourseForm')">Add Semester</button>
+<a href="logout.php"> <button>Logout</button> </a>
 </html>
