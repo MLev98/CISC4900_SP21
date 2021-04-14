b006934c195d07aafffa0940c6a873ab3d7a1d7f
MLev98 <58785503+MLev98@users.noreply.github.com>
2021-04-14 17:21:16 -0400

    File Organization
    * Made CSS & JS Folders in the src folder for more organization

--- src/dashboard.php
+++ src/dashboard.php
@@ -13,8 +13,8 @@ include("config.php");
 <!-- BOOTSTRAP CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
-<link rel="stylesheet" href="sortable.css">
-<script src="sortable.js"></script>
+<link rel="stylesheet" href="css/sortable.css">
+<script src="js/sortable.js"></script>
 
 <!-- ADD A COURSE FORM -->
 <?php include('endpoints/addCourseEndpoint.php');?>
