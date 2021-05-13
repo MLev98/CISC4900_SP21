b25075b04477c30f01522f84ad22053151defa6b
MLev98 <58785503+MLev98@users.noreply.github.com>
2021-05-12 20:24:12 -0400

    Documentation (#12)
    * Documentation
    
    * Initial Documentation for our JavaScript Functions
    (run via index.html)
    
    * Fixes Sortable Documentation
    
    * Sortable was not properly documented.
    * Updates the documentation html's.
    
    * File Organization
    
    * Move out folder and rename to docs one level up.
    
    * Perhaps this one works better.
    
    * Revisit Valid Password Regex
    
    * Update notifications.js
    
    jsDocs
    
    * Updates Documentation
    
    * Updates Documentation due to @Cgmcallister98 annotating the sortable.js
    
    Co-Authored-By: Cassidy McAllister <cgmcallister98@gmail.com>
    
    * Reformat notifications.js
    
    * Lines were not properly formatted on the script file, this commit addresses the issue.
    
    Co-Authored-By: Cassidy McAllister <cgmcallister98@gmail.com>
    
    * Oops
    
    Fixes location of documentation
    
    * Updates README.md Documentation
    
    * Updates README.md Documentation
    
    Co-Authored-By: lrojas998 <61262142+lrojas998@users.noreply.github.com>
    
    * Fixes Display of 2nd Tutorial Image
    
    * Used png instead of jpg
    * Fixed a few typos in my explanation.
    
    * Touches up on README.md Documentation
    
    * Thanks to feedback from @Cgmcallister98 corrects Step 2.
    
    Co-Authored-By: Cassidy McAllister <cgmcallister98@gmail.com>
    
    * Touches up on README.md 2
    
    * Missing a couple of words.
    
    Co-Authored-By: Cassidy McAllister <cgmcallister98@gmail.com>
    
    * Update README.md
    
    * Actually Removes Third Image
    
    * Removes Third Image
    
    Co-authored-by: Cassidy McAllister <cgmcallister98@gmail.com>
    Co-authored-by: lrojas998 <61262142+lrojas998@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# CISC4900_SP21
-A group project Web Dev App.
+# CIS Registration Assistant
+
+## Live Hosted
+
+To view the live hosted version of this repository click on [this](https://cisc4900.herokuapp.com/) link.
+
+## Local Installation Guide
+
+1. To run the application locally, you will need to download the source code & install **XAMPP Control Panel** using [this](https://www.apachefriends.org/download.html) link.
+
+2. You will then need to copy the **CISC4900_SP21** folder onto the same drive you installed **XAMPP Control Panel** into the path:
+
+```
+\xampp\htdocs
+```
+
+3. Run **XAMPP** and click *Start for Apache and MySQL*
+
+4. Click **Admin** for *MySQL* and follow the screenshots below to setup the database:
+
+![Importing into the Current Server](./img/MySQL_Install_1.png)
+
+![How to Import our Database](./img/MySQL_Install_2.jpg)
+
+5. Click **Admin** for *Apache* and this will open a webpage:
+
+```
+http://localhost/dashboard/
+```
+
+Change the URL to:
+
+```
+http://localhost/CISC4900_SP21
+```
+
+6. From here you can click on the **src** folder to test the application.
--- src/js/notifications.js
+++ src/js/notifications.js
@@ -1,50 +1,74 @@
 /* sweet alert notifications */
 
+/**
+* Notifies user of successful login message and redirects user to main page.
+*
+*/
 function logged_in_notification() {
-    Swal.fire({
+  Swal.fire({
     title: '<strong>Successfully logged in</strong>',
     icon: 'success'
   }).then((result) => {
     if (result.isConfirmed) {
       window.location.href = "dashboard.php";
-  }
+    }
   })
-  }
-  
-  function wrong_log_in_notification() {
-    Swal.fire({
+}
+
+/**
+* Notifies user of Invalid credentials error
+*
+*/
+function wrong_log_in_notification() {
+  Swal.fire({
     title: '<strong>Invalid Username or Password</strong>',
     icon: 'warning'
   })
-  }
-  
-  function username_taken_notification() {
-    Swal.fire({
+}
+
+/**
+* Notifies user of Username taken error
+*
+*/
+function username_taken_notification() {
+  Swal.fire({
     title: '<strong>Username taken, try again</strong>',
     icon: 'warning'
   })
-  }
-  
-  function successfully_registered_notification() {
-    Swal.fire({
+}
+
+/**
+* Notifies user of Successful creation of account, and redirtects user to main page
+*
+*/
+function successfully_registered_notification() {
+  Swal.fire({
     title: '<strong>Successfully registered</strong>',
     icon: 'success'
   }).then((result) => {
     if (result.isConfirmed) {
       window.location.href = "dashboard.php";
-  }
+    }
   })
-  }
-  
-  function passwords_dont_match_notification() {
-    Swal.fire({
+}
+
+/**
+* Notifies user of Miss Matched Password error
+*
+*/
+function passwords_dont_match_notification() {
+  Swal.fire({
     title: '<strong>Passwords dont match, try again</strong>',
     icon: 'warning'
   })
-  }
-  
-  function successfully_delete_course_notification() {
-    Swal.fire({
+}
+
+/**
+* Notifies user of Course Deleted and updates page if notification is approved
+*
+*/
+function successfully_delete_course_notification() {
+  Swal.fire({
     title: '<strong>Successfully deleted</strong>',
     icon: 'success',
     iconColor: 'red',
@@ -52,35 +76,47 @@ function logged_in_notification() {
   }).then((result) => {
     if (result.isConfirmed) {
       window.location.href = "dashboard.php";
-  }
+    }
   })
-  }
+}
 
-  function successfully_added_course_notification() {
-    Swal.fire({
+/**
+* Notifies user of Course added and updates page if notification is approved
+*
+*/
+function successfully_added_course_notification() {
+  Swal.fire({
     title: '<strong>Successfully added</strong>',
     icon: 'success'
   }).then((result) => {
     if (result.isConfirmed) {
       window.location.href = "dashboard.php";
-  }
+    }
   })
-  }
+}
 
-  function unsuccessfully_added_course_notification() {
-    Swal.fire({
+/**
+* Notifies user of Course not added error
+*
+*/
+function unsuccessfully_added_course_notification() {
+  Swal.fire({
     title: '<strong>Credit Limit Overpassed</strong>',
     icon: 'error'
   }).then((result) => {
     if (result.isConfirmed) {
       window.location.href = "dashboard.php";
-  }
+    }
   })
-  }
+}
 
-  function no_dept_class_selected() {
-    Swal.fire({
+/**
+* Notifies user to select a department and class with an error message
+*
+*/
+function no_dept_class_selected() {
+  Swal.fire({
     title: '<strong>Select a department and class</strong>',
     icon: 'warning'
   })
-  }
+}
